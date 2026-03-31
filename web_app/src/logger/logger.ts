cbe93e6177ee440d4382a1f608ed00fa6a08000a
WilsonnnTan <wilsonangelietann@gmail.com>
2026-04-01 02:38:28 +0700

    fix: remove file creation on logger

--- web_app/src/logger/logger.ts
+++ web_app/src/logger/logger.ts
@@ -1,44 +1,13 @@
 import { Prisma } from '@/generated/prisma/client';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import winston from 'winston';
-import 'winston-daily-rotate-file';
 
 const { combine, timestamp, json, colorize, align, errors } = winston.format;
-
-const __filename = fileURLToPath(import.meta.url);
-const logDir = path.dirname(__filename);
-
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
-}
-
-// Define transports file for combined logs and error logs with daily rotation
-const combinedTransport = new winston.transports.DailyRotateFile({
-  filename: path.join(logDir, 'combined-%DATE%.log'),
-  datePattern: 'YYYY-MM-DD',
-  maxFiles: '7d',
-  format: combine(errors({ stack: true }), timestamp(), json()),
-});
-
-const errorTransport = new winston.transports.DailyRotateFile({
-  filename: path.join(logDir, 'error-%DATE%.log'),
-  datePattern: 'YYYY-MM-DD',
-  level: 'error',
-  maxFiles: '7d',
-  format: combine(errors({ stack: true }), timestamp(), json()),
-});
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   defaultMeta: { service: 'user-service' },
-  transports: [combinedTransport, errorTransport],
-});
-
-// terminal logging for development
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
+  format: combine(errors({ stack: true }), timestamp(), json()),
+  transports: [
     new winston.transports.Console({
       format: combine(
         errors({ stack: true }),
@@ -47,8 +16,8 @@ if (process.env.NODE_ENV !== 'production') {
         align(),
       ),
     }),
-  );
-}
+  ],
+});
 
 const prisma_error_instance = [
   Prisma.PrismaClientKnownRequestError,
